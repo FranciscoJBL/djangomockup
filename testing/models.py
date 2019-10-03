41517601603f1d8f9995fd18a8a5459ecded2bb0
Francisco Briones <francisco.briones.l@gmail.com>
2019-10-03 17:50:27 -0300

    added base files

---
+++ testing/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    creation = models.DateTimeField('date created')
+
+    def __str__():
+        return self.username
+
+class Article(models.Model):
+    content = models.CharField(max_length=1200)
+    publication_date = models.DateTimeField('date published')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    publication_name = models.CharField(max_length=30)
+
+    def __str__():
+        return self.publication_name
+
